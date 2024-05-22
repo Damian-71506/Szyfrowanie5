a5a5d4d517931e807c8aebcf65feb88ff88b7c97
Damian 71506 <gdx162129@student.gdansk.merito.pl>
2024-05-22 19:31:04 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Szyfrowanie5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDrawSin = new System.Windows.Forms.Button();
+            this.btnDrawCosinus = new System.Windows.Forms.Button();
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnDrawSin
+            // 
+            this.btnDrawSin.Location = new System.Drawing.Point(36, 31);
+            this.btnDrawSin.Name = "btnDrawSin";
+            this.btnDrawSin.Size = new System.Drawing.Size(87, 23);
+            this.btnDrawSin.TabIndex = 0;
+            this.btnDrawSin.Text = "Draw Sinus";
+            this.btnDrawSin.UseVisualStyleBackColor = true;
+            this.btnDrawSin.Click += new System.EventHandler(this.btnDrawSin_Click);
+            // 
+            // btnDrawCosinus
+            // 
+            this.btnDrawCosinus.Location = new System.Drawing.Point(36, 78);
+            this.btnDrawCosinus.Name = "btnDrawCosinus";
+            this.btnDrawCosinus.Size = new System.Drawing.Size(87, 23);
+            this.btnDrawCosinus.TabIndex = 1;
+            this.btnDrawCosinus.Text = "Draw Cosinus";
+            this.btnDrawCosinus.UseVisualStyleBackColor = true;
+            this.btnDrawCosinus.Click += new System.EventHandler(this.btnDrawCosinus_Click);
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.Location = new System.Drawing.Point(235, 31);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(492, 349);
+            this.panelDraw.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDraw);
+            this.Controls.Add(this.btnDrawCosinus);
+            this.Controls.Add(this.btnDrawSin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDrawSin;
+        private System.Windows.Forms.Button btnDrawCosinus;
+        private System.Windows.Forms.Panel panelDraw;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szyfrowanie5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Obsługa zdarzenia dla przycisku "Draw Sinus"
+        private void btnDrawSin_Click(object sender, EventArgs e)
+        {
+           DrawGraph(Math.Sin, Color.Blue);
+        }
+
+        // Obsługa zdarzenia dla przycisku "Draw Cosinus"
+        private void btnDrawCosinus_Click(object sender, EventArgs e)
+        {
+            DrawGraph(Math.Cos, Color.Red);
+        }
+
+        // Metoda do rysowania wykresu funkcji trygonometrycznej
+        private void DrawGraph(Func<double, double> trigFunction, Color color)
+        {
+            Bitmap bitmap = new Bitmap(panelDraw.Width, panelDraw.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White);
+
+                Pen axisPen = new Pen(Color.Black, 2);
+                Pen graphPen = new Pen(color, 2);
+
+                // Rysowanie osi
+                g.DrawLine(axisPen, 0, panelDraw.Height / 2, panelDraw.Width, panelDraw.Height / 2);
+                g.DrawLine(axisPen, panelDraw.Width / 2, 0, panelDraw.Width / 2, panelDraw.Height);
+
+                // Rysowanie wykresu
+                for (int x = -panelDraw.Width / 2; x < panelDraw.Width / 2; x++)
+                {
+                    double radians = x * (Math.PI / 180);
+                    double y = trigFunction(radians);
+
+                    int screenX = x + panelDraw.Width / 2;
+                    int screenY = (int)(-y * 50 + panelDraw.Height / 2); // Skalowanie i przesunięcie
+
+                    if (x != -panelDraw.Width / 2)
+                    {
+                        g.DrawLine(graphPen, previousScreenX, previousScreenY, screenX, screenY);
+                    }
+
+                    previousScreenX = screenX;
+                    previousScreenY = screenY;
+                }
+            }
+            panelDraw.BackgroundImage = bitmap;
+        }
+
+        private int previousScreenX;
+        private int previousScreenY;
+    }
+}
